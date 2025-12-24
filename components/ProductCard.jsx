d497515b87119c2cb6ae4affb7b31838338d219a
Sarah <sarahhumayra28@gmail.com>
2025-12-24 20:43:10 +0600

    FrontEnd Commit

---
+++ components/ProductCard.jsx
@@ -0,0 +1,42 @@
+'use client'
+import { StarIcon } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const ProductCard = ({ product }) => {
+
+    const currency = process.env.NEXT_PUBLIC_CURRENCY_SYMBOL || ' ৳ ' 
+
+    // calculate the average rating of the product
+    const rating = Math.round(product.rating.reduce((acc, curr) => acc + curr.rating, 0) / product.rating.length);
+
+    return (
+        <Link href={`/product/${product.id}`} className=' group max-xl:mx-auto'>
+            <div className="bg-gray-100 rounded-lg flex items-center justify-center overflow-hidden w-60 h-60 group">
+            <Image
+                src={product.images[0]}
+                alt={product.name}
+                width={800}
+                height={800}
+                className="object-contain w-full h-full transition-transform duration-300 group-hover:scale-110"
+            />
+            </div>
+
+
+            <div className='flex justify-between gap-3 text-sm text-slate-800 pt-2 max-w-60'>
+                <div>
+                    <p>{product.name}</p>
+                    <div className='flex'>
+                        {Array(5).fill('').map((_, index) => (
+                            <StarIcon key={index} size={14} className='text-transparent mt-0.5' fill={rating >= index + 1 ? "#00C950" : "#D1D5DB"} />
+                        ))}
+                    </div>
+                </div>
+                <p>{currency}{product.price}</p>
+            </div>
+        </Link>
+    )
+}
+
+export default ProductCard
